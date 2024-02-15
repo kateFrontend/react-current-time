65fc48739f4787f603289e5d7babbbc87986f5ef
Kate Danilova <ecdanilova@gmail.com>
2024-02-14 20:13:43 -0500

    add simple css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>My current time</h1>
+      <Clock/>
     </div>
   );
 }
