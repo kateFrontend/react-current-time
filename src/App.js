9e60d2cd36726e09c763c34c519da2b24d3703a7
zishan07cse <zishan07csecuet@gmail.com>
2024-04-01 00:53:38 +0600

    Fix:App.js file with current date

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Clock from './Clock';
 function App() {
   return (
     <div className="App">
-      {/* <h1>My current date</h1>
-      <Date/> */}
+      <h1>My current date</h1>
+      <Date/>
       <h1>My current time</h1>
       <Clock/>
     </div>
